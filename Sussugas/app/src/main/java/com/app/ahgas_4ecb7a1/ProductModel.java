c67c4c7b78caf01cc2b91f438af5814107636822
saifal <saifal@saifals-MacBook-Pro.local>
2020-12-15 06:48:46 +0500

    mari and sussu

---
+++ Sussugas/app/src/main/java/com/app/ahgas_4ecb7a1/ProductModel.java
@@ -0,0 +1,17 @@
+package com.app.ahgas_4ecb7a1;
+
+public class ProductModel {
+    String name;
+
+    public ProductModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Sussugas/app/src/main/java/com/app/ahgas_4ecb7a1/RatingModel.java
@@ -0,0 +1,13 @@
+package com.app.ahgas_4ecb7a1;
+
+public class RatingModel {
+    double rating;
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
