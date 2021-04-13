2acf52a5647ce57ab7e798e232fd5c324b36635b
saifal <saifal@saifals-MacBook-Pro.local>
2021-04-14 04:02:32 +0500

    update

---
+++ Avenida/app/src/main/java/com/app/ahgas_e9cd797/RatingModel.java
@@ -0,0 +1,13 @@
+package com.app.ahgas_e9cd797;
+
+public class RatingModel {
+    double rating;
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
